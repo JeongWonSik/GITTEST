3114944c0a0bc9a92e9480a53a31b408d2bf8cf8
fbt0159 <fbt0159@naver.com>
2021-06-09 11:46:12 +0900

    Master ver 0.1

--- src/Ch01/C01Main.java
+++ src/Ch01/C01Main.java
@@ -3,8 +3,8 @@ package Ch01;
 public class C01Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Master Gittest 01");
+		System.out.println("Master Gittest 02");
 	}
 
 }
